78e3a2a6870d684dfe44e706d83c1c63839f1941
victorcuaresma-pe <71667340@undc.edu.pe>
2025-05-16 06:29:08 +0000

    actualizacion2

---
+++ target/classes/sql/users.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS usuarios (
+                                        id SERIAL PRIMARY KEY,
+                                        nombre VARCHAR(100),
+    correo VARCHAR(100)
+    );
